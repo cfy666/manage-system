83f502c3baea73f1bd0859f8ee4c0c6d73605ca5
cfy <1263473716@qq.com>
2021-01-30 00:19:21 +0800

    koa2跨域请求配置和编写登录接口以及抽离返回信息集合

--- txclass_manager/src/components/Login/index.js
+++ txclass_manager/src/components/Login/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import axios from 'axios';
+
 import './index.scss';
 
 import Logo from './Logo';
 import Form from './Form';
 export default class Login extends Component {
+
+  getCourseData () {
+    axios({
+      url: 'http://localhost:3000/admin/login_action',
+      method: 'post',
+      data: {
+        password: 'admin',
+        username: 'admin'
+      }
+    }).then((res) => {
+      console.log(res.data);
+    })
+  }
+
+  componentDidMount () {
+    this.getCourseData();
+  }
+
   render () {
     return (
       <div className="login-container">
